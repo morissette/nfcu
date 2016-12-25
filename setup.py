c2f185d8f5a71e4e95e7dca50d83cdd0277ea6d4
Matthew Harris <mharris@doublehorn.com>
2016-12-25 15:39:20 -0600

    Update packages

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+from setuptools import find_packages
+
 setup(
   name = 'nfcu',
-  packages = ['nfcu'],
-  version = '0.1.1',
+  packages = find_packages(),
+  version = '0.1.2',
   description = 'Navy Federal Credit Union Module',
   author = 'Matthew Harris',
   author_email = 'matt@x-qa.com',
   url = 'https://github.com/morissette/nfcu',
-  download_url = 'https://github.com/morissette/nfcu/tarball/0.1.1',
+  download_url = 'https://github.com/morissette/nfcu/tarball/0.1.2',
   keywords = ['nfcu', 'fintech', 'navy federal', 'api'],
   classifiers = [],
 )
